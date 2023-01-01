7c6a9a919e3bcecab9a4bd2f4bf341e86a942469
Aleksandr Zahorodnii <git.hub.zagar@gmail.com>
2023-01-01 21:40:11 +0100

    this vs super

---
+++ src/mate/academy/Cat.java
@@ -0,0 +1,22 @@
+package mate.academy;
+
+public class Cat extends Animal {
+    private String color;
+
+    public Cat(String name, int age) {
+        super(name, age);
+    }
+
+    public Cat(String name, int age, String color) {
+        super (name, age);
+        this.color = color;
+    }
+
+    @Override
+    public String toString() {
+        return "Cat{" +
+                " name='" + getName() + '\'' +
+                " age='" + getAge() + '\'' +
+                " color='" + color + '\'' + '}';
+    }
+}
